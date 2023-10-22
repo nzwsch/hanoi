1c9a9a64a3817f0f5c9a07a3f84ed022671c2404
Seiichi Yonezawa <nzwsch@gmail.com>
2023-10-23 05:08:23 +0900

    Update hanoi.spec.js

--- tests/hanoi.spec.js
+++ tests/hanoi.spec.js
@@ -1,14 +1,35 @@
 // @ts-check
 const { test, expect } = require("@playwright/test");
 
-test("has title", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("/");
+});
+
+test("has title", async ({ page }) => {
   await expect(page).toHaveTitle("hanoi");
 });
 
-test("test cancel pegs", async ({ page }) => {
-  await page.goto("/");
+test("#peg-a has 8 disks", async ({ page }) => {
+  let disks = await page.locator("#peg-a > .disk");
+  await expect(disks).toHaveCount(8);
+});
 
+test("#peg-b has 0 disks", async ({ page }) => {
+  let disks = await page.locator("#peg-b > .disk");
+  await expect(disks).toHaveCount(0);
+});
+
+test("#peg-c has 0 disks", async ({ page }) => {
+  let disks = await page.locator("#peg-c > .disk");
+  await expect(disks).toHaveCount(0);
+});
+
+test("buttons have 3 buttons", async ({ page }) => {
+  let buttons = await page.locator(".buttons > button");
+  await expect(buttons).toHaveCount(3);
+});
+
+test("test A->A pegs", async ({ page }) => {
   let pegA = await page.locator("#peg-a");
   let pegB = await page.locator("#peg-b");
   let pegC = await page.locator("#peg-c");
@@ -30,9 +51,7 @@ test("test cancel pegs", async ({ page }) => {
   await expect(pegC).not.toHaveClass(/selected/);
 });
 
-test("test cacnel buttons", async ({ page }) => {
-  await page.goto("/");
-
+test("test A->A buttons", async ({ page }) => {
   let buttonA = await page.locator("#button-a");
   let buttonB = await page.locator("#button-b");
   let buttonC = await page.locator("#button-c");
@@ -66,9 +85,7 @@ test("test cacnel buttons", async ({ page }) => {
   await expect(buttonC).toBeDisabled();
 });
 
-test("test move pegs", async ({ page }) => {
-  await page.goto("/");
-
+test("test A->B->A pegs", async ({ page }) => {
   let pegA = await page.locator("#peg-a");
   let pegB = await page.locator("#peg-b");
   let pegC = await page.locator("#peg-c");
@@ -92,9 +109,7 @@ test("test move pegs", async ({ page }) => {
   await expect(pegC).not.toHaveClass(/selected/);
 });
 
-test("test move buttons", async ({ page }) => {
-  await page.goto("/");
-
+test.skip("test A->B->A buttons", async ({ page }) => {
   let buttonA = await page.locator("#button-a");
   let buttonB = await page.locator("#button-b");
   let buttonC = await page.locator("#button-c");
@@ -127,5 +142,65 @@ test("test move buttons", async ({ page }) => {
 
   await expect(buttonA).not.toBeDisabled();
   await expect(buttonB).toBeDisabled();
+  await expect(buttonC).not.toBeDisabled();
+});
+
+test("test A->B->B pegs", async ({ page }) => {
+  let pegA = await page.locator("#peg-a");
+  let pegB = await page.locator("#peg-b");
+  let pegC = await page.locator("#peg-c");
+
+  await page.getByRole("button", { name: "A" }).click();
+
+  await expect(pegA).toHaveClass(/selected/);
+  await expect(pegB).not.toHaveClass(/selected/);
+  await expect(pegC).not.toHaveClass(/selected/);
+
+  await page.getByRole("button", { name: "B" }).click();
+
+  await expect(pegA).not.toHaveClass(/selected/);
+  await expect(pegB).not.toHaveClass(/selected/);
+  await expect(pegC).not.toHaveClass(/selected/);
+
+  await page.getByRole("button", { name: "B" }).click();
+
+  await expect(pegA).not.toHaveClass(/selected/);
+  await expect(pegB).toHaveClass(/selected/);
+  await expect(pegC).not.toHaveClass(/selected/);
+});
+
+test.skip("test A->B->B buttons", async ({ page }) => {
+  let buttonA = await page.locator("#button-a");
+  let buttonB = await page.locator("#button-b");
+  let buttonC = await page.locator("#button-c");
+
+  await page.getByRole("button", { name: "A" }).click();
+
+  await expect(buttonA).toHaveClass(/selected/);
+  await expect(buttonB).not.toHaveClass(/selected/);
+  await expect(buttonC).not.toHaveClass(/selected/);
+
+  await expect(buttonA).not.toBeDisabled();
+  await expect(buttonB).not.toBeDisabled();
+  await expect(buttonC).not.toBeDisabled();
+
+  await page.getByRole("button", { name: "B" }).click();
+
+  await expect(buttonA).not.toHaveClass(/selected/);
+  await expect(buttonB).not.toHaveClass(/selected/);
+  await expect(buttonC).not.toHaveClass(/selected/);
+
+  await expect(buttonA).not.toBeDisabled();
+  await expect(buttonB).not.toBeDisabled();
+  await expect(buttonC).toBeDisabled();
+
+  await page.getByRole("button", { name: "B" }).click();
+
+  await expect(buttonA).not.toHaveClass(/selected/);
+  await expect(buttonB).toHaveClass(/selected/);
+  await expect(buttonC).not.toHaveClass(/selected/);
+
+  await expect(buttonA).not.toBeDisabled();
+  await expect(buttonB).not.toBeDisabled();
   await expect(buttonC).not.toBeDisabled();
 });
