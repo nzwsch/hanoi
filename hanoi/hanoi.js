f0120cf8fa51fa132ab8588a79d2fbe2fbfabc07
Seiichi Yonezawa <nzwsch@gmail.com>
2023-10-23 06:24:41 +0900

    Refactor hanoi.js

--- hanoi/hanoi.js
+++ hanoi/hanoi.js
@@ -1,94 +1,102 @@
 (() => {
-  const disableButtonsIfPegIsEmpty = (buttons) => {
-    buttons.forEach((button) => {
+  class Hanoi {
+    constructor() {
+      this.hanoi = document.getElementById("hanoi");
+      this.buttons = this.hanoi.querySelectorAll(".buttons > button");
+    }
+
+    setup() {
+      this.hanoi.querySelectorAll(".disk").forEach((disk, index) => {
+        disk.setAttribute("data-index", index);
+      });
+
+      const buttonA = document.getElementById("button-a");
+      const buttonB = document.getElementById("button-b");
+      const buttonC = document.getElementById("button-c");
+
+      this.setupButton(buttonA, "peg-a");
+      this.setupButton(buttonB, "peg-b");
+      this.setupButton(buttonC, "peg-c");
+
+      buttonA.removeAttribute("disabled");
+    }
+
+    setupButton(button, dataTarget) {
+      button.setAttribute("disabled", true);
+      button.setAttribute("data-target", dataTarget);
+      button.addEventListener("click", (event) => this.clickHandler(event));
+    }
+
+    clickHandler(event) {
+      const button = event.target;
       const peg = document.getElementById(button.dataset.target);
-
-      if (peg.children.length === 0) {
-        button.setAttribute("disabled", true);
-      } else {
-        button.removeAttribute("disabled");
-      }
-    });
-  };
-
-  const disableButtonsIfPegCantMove = (buttons, clickedPeg) => {
-    buttons.forEach((button) => {
-      if (button.classList.contains("selected")) {
-        return;
-      }
-      const peg = document.getElementById(button.dataset.target);
-      if (peg.children.length === 0) {
-        button.removeAttribute("disabled");
-        return;
-      }
-      const diskA = clickedPeg.querySelector(".disk:first-child");
-      const diskB = peg.querySelector(".disk:first-child");
-      if (parseInt(diskA.dataset.index) > parseInt(diskB.dataset.index)) {
-        button.setAttribute("disabled", true);
-      } else {
-        button.removeAttribute("disabled");
-      }
-    });
-  };
-
-  const moveDiskFromSelectedToClicked = (buttons, clickedPeg) => {
-    const selected = document.querySelector(".buttons .selected");
-    const selectedPeg = document.getElementById(selected.dataset.target);
-    const disk = selectedPeg.querySelector(".disk:first-child");
-
-    selectedPeg.classList.remove("selected");
-    clickedPeg.prepend(disk);
-    buttons.forEach((button) => (button.className = ""));
-    disableButtonsIfPegIsEmpty(buttons);
-  };
-
-  const clickButton = (pegId) => {
-    const hanoi = document.getElementById("hanoi");
-    const buttons = hanoi.querySelectorAll("button");
-    const peg = document.getElementById(pegId);
-
-    return (event) => {
-      const clicked = event.target;
-      const isCanceled = clicked.classList.contains("selected");
-      const isNotSelected = Array.from(buttons).every(
-        (btn) => btn.className === ""
+      const isCanceled = button.classList.contains("selected");
+      const isNotSelected = Array.from(this.buttons).every(
+        (button) => !button.classList.contains("selected")
       );
 
       if (isCanceled) {
-        clicked.classList.remove("selected");
+        button.classList.remove("selected");
         peg.classList.remove("selected");
-        disableButtonsIfPegIsEmpty(buttons);
+        this.disableButtonsIfPegIsEmpty();
       } else if (isNotSelected) {
-        clicked.classList.add("selected");
+        button.classList.add("selected");
         peg.classList.add("selected");
-        disableButtonsIfPegCantMove(buttons, peg);
+        this.disableButtonsIfPegCantMove(peg);
       } else {
-        // clicked.classList.remove("clicked");
-        // peg.classList.remove("clicked");
-        moveDiskFromSelectedToClicked(buttons, peg);
+        this.moveDiskFromSelectedToClicked(peg);
+        this.buttons.forEach((button) => button.classList.remove("selected"));
+        this.disableButtonsIfPegIsEmpty();
       }
-    };
-  };
+    }
 
+    disableButtonsIfPegIsEmpty() {
+      this.buttons.forEach((button) => {
+        const peg = document.getElementById(button.dataset.target);
+
+        if (peg.children.length === 0) {
+          button.setAttribute("disabled", true);
+        } else {
+          button.removeAttribute("disabled");
+        }
+      });
+    }
+
+    disableButtonsIfPegCantMove(selectedPeg) {
+      this.buttons.forEach((button) => {
+        if (button.classList.contains("selected")) {
+          return;
+        }
+
+        const peg = document.getElementById(button.dataset.target);
+
+        if (peg.children.length === 0) {
+          button.removeAttribute("disabled");
+          return;
+        }
+
+        const diskA = selectedPeg.querySelector(".disk:first-child");
+        const diskB = peg.querySelector(".disk:first-child");
+
+        if (parseInt(diskA.dataset.index) > parseInt(diskB.dataset.index)) {
+          button.setAttribute("disabled", true);
+        } else {
+          button.removeAttribute("disabled");
+        }
+      });
+    }
+
+    moveDiskFromSelectedToClicked(clickedPeg) {
+      const selected = document.querySelector(".buttons .selected");
+      const selectedPeg = document.getElementById(selected.dataset.target);
+      const disk = selectedPeg.querySelector(".disk:first-child");
+
+      clickedPeg.prepend(disk);
+      selectedPeg.classList.remove("selected");
+    }
+  }
   document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll(".disk").forEach((disk, index) => {
-      disk.setAttribute("data-index", index);
-    });
-
-    const buttonA = document.getElementById("button-a");
-    const buttonB = document.getElementById("button-b");
-    const buttonC = document.getElementById("button-c");
-
-    buttonA.removeAttribute("disabled");
-    buttonB.setAttribute("disabled", true);
-    buttonC.setAttribute("disabled", true);
-
-    buttonA.setAttribute("data-target", "peg-a");
-    buttonB.setAttribute("data-target", "peg-b");
-    buttonC.setAttribute("data-target", "peg-c");
-
-    buttonA.addEventListener("click", clickButton("peg-a"));
-    buttonB.addEventListener("click", clickButton("peg-b"));
-    buttonC.addEventListener("click", clickButton("peg-c"));
+    const hanoi = new Hanoi();
+    hanoi.setup();
   });
 })();
